7149a43a22b76d00bee08e7724045e7862a8e54a
Haris <harisgujjar317@gmail.com>
2024-06-23 02:36:38 -0400

    quote button

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -22,10 +22,10 @@ const Home = () => {
           <h1 className="mt-10 text-2xl sm:text-3xl md:text-5xl text-white font-semibold leading-tight ">Transforming  <span className="text-brand-orange">Ideas</span> into Seamless <br className='hidden md:block' /> Digital Experiences</h1>
           <p className="mt-10 text-base sm:text-lg md:text-2xl text-brand-gray">Founded in 2023, Soitrix has established itself as a premier full-service web development agency dedicated to crafting exceptional digital experiences. Our multidisciplinary team of talented developers, designers, and strategists collaborates seamlessly to bring visionary ideas to life. With a steadfast commitment to excellence, we meticulously tailor each project to exceed the highest standards of quality and creativity.</p>
           <div className='flex justify-center mt-16'>
-            <button onClick={() => handleScroll('contact')} className='bg-brand-gray-100 p-2.5 w-auto flex justify-center gap-10 border-[2px] border-gray-50 rounded-lg'>
-              <span className='text-white text-2xl mt-2'>Get a Quote</span>
-              <div className='bg-gray-100 rounded-lg h-12 w-14 flex justify-center'>
-                <img src={arrow} className='mt-2 h-8 w-7' alt="" />
+            <button onClick={() => handleScroll('contact')} className='bg-brand-gray-100 py-1.5 px-2.5 w-auto flex justify-center gap-10 border-[2px] border-gray-50 rounded-xl'>
+              <span className='text-white text-2xl mt-3'>Get a Quote</span>
+              <div className='bg-gray-100 rounded-lg h-14 w-16 flex justify-center'>
+                <img src={arrow} className='mt-3 h-8 w-7' alt="" />
               </div>
             </button>  
           </div>        
